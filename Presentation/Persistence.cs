85da6d6ffa77dcba5bab394be6e2edebfe7ed27d
kooken12 <aymeric.samyn@live.be>
2018-08-28 19:13:36 +0200

    UsernameAvailable & emailAvailable (juist nog MySQL code)

--- Presentation/Persistence.cs
+++ Presentation/Persistence.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Presentation
 {
@@ -12,6 +13,58 @@ namespace Presentation
         {
             _connectionString = "server=localhost;user id=root;database=poker;Password='Test123'";
         }
-        Persistence
+        public Boolean userNameAvailable(string username)
+        {
+            Boolean result = false;
+
+            MySqlConnection conn = new MySqlConnection(_connectionString);
+            MySqlCommand cmd = new MySqlCommand(/*COMMAND*/, conn);
+
+            conn.Open();
+                       
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            string strResult = string.Empty;
+            strResult = Convert.ToString(cmd.ExecuteScalar());
+
+            if(strResult == string.Empty)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            conn.Close();
+
+            return result;
+        }
+
+        public Boolean emailAvailable(string email)
+        {
+            Boolean result = false;
+
+            MySqlConnection conn = new MySqlConnection(_connectionString);
+            MySqlCommand cmd = new MySqlCommand(/*COMMAND*/, conn);
+
+            conn.Open();
+
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            string strResult = string.Empty;
+            strResult = Convert.ToString(cmd.ExecuteScalar());
+
+            if (strResult == string.Empty)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            conn.Close();
+
+            return result;
+        }
     }
 }
